ce51878b3928109ae57d162e4bd2b3ba8fb0cac3
Ujjwal Sharma <ujjwalsharma@protonmail.com>
2017-12-16 11:59:56 +0100

    Added frankenstein digit generation and fixes.

--- a3_simple_template.py
+++ a3_simple_template.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 from datetime import datetime
+import matplotlib
+
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -9,8 +12,6 @@ os.makedirs(SUMMARY_PATH, exist_ok=True)
 
 IMG_DIR = './plots/'
 os.makedirs(IMG_DIR, exist_ok=True)
-
-IDENTIFIER = "NB"
 
 OPTIMIZER_DICT = {'sgd': tf.train.GradientDescentOptimizer,  # Gradient Descent
                   'adadelta': tf.train.AdadeltaOptimizer,  # Adadelta
@@ -60,7 +61,7 @@ class NaiveBayesModel(object):
         self.n_dims = w_init[1]
         self.n_labels = w_init[0]
 
-        self.path = IMG_DIR + IDENTIFIER + datetime.now().strftime("%Y-%m-%d %H:%M") + '/'
+        self.path = IMG_DIR + datetime.now().strftime("%Y-%m-%d %H:%M") + '/'
         os.makedirs(IMG_DIR + datetime.now().strftime("%Y-%m-%d %H:%M"), exist_ok=True)
 
     def log_p_x_given_z(self, x, z=None):
@@ -121,23 +122,20 @@ class NaiveBayesModel(object):
                 0, 1, sampled_latent_variables[idx], color='black', backgroundcolor='white', fontsize=8)
             plt.imshow(samples[idx].reshape(28, 28), cmap='gray')
             plt.axis('off')
-        plt.savefig('./NB_%s.png' % str(i))
+        plt.savefig(self.path + 'NB_%s.png' % str(i))
         plt.close()
 
     def sample_K_latent_variables(self):
 
         prob = tf.sigmoid(self.w + self.c)
         samples = np.zeros((self.n_categories, self.n_dims))
+
         sampled_latent_variables = list()
 
         for n in range(self.n_categories):
             # Sample a random latent state
-            z = n
-            sampled_latent_variables.append(z)
-
-            # Extract Bernoulli Pixel distribution for sampled latent variable.
-            dist = tf.distributions.Bernoulli(probs=prob[z])
-            samples[n] = dist.sample(1).eval()
+            samples[n] = prob[n].eval()
+            sampled_latent_variables.append(n)
 
         return samples, sampled_latent_variables
 
@@ -150,7 +148,7 @@ class NaiveBayesModel(object):
                 0, 1, sampled_latent_variables[idx], color='black', backgroundcolor='white', fontsize=8)
             plt.imshow(samples[idx].reshape(28, 28), cmap='gray')
             plt.axis('off')
-        plt.savefig('./K_NB_%s.png' % str(i))
+            plt.savefig(self.path + 'NB_K_Samples_%s.png' % str(i))
         plt.close()
 
     def train_step(self, loss, flags):
@@ -164,7 +162,7 @@ class NaiveBayesModel(object):
 
 
 def train_simple_generative_model_on_mnist(n_categories=20, initial_mag=0.01, optimizer='rmsprop', learning_rate=.01,
-                                           n_epochs=20, test_every=1000,
+                                           n_epochs=500, test_every=100,
                                            minibatch_size=100, plot_n_samples=16):
     """
     Train a simple Generative model on MNIST and plot the results.
@@ -181,10 +179,17 @@ def train_simple_generative_model_on_mnist(n_categories=20, initial_mag=0.01, op
 
     # Get Data
     x_train, x_test = load_mnist_images(binarize=True)
+    n_samples, n_dims = x_train.shape
+
+    # Training Set Iterator
     train_iterator = tf.data.Dataset.from_tensor_slices(
         x_train).repeat().batch(minibatch_size).make_initializable_iterator()
-    n_samples, n_dims = x_train.shape
     x_minibatch = train_iterator.get_next()  # Get symbolic data, target tensors
+
+    # Test Set Iterator
+    test_iterator = tf.data.Dataset.from_tensor_slices(x_test).repeat().batch(
+        minibatch_size).make_initializable_iterator()
+    x_test_minibatch = test_iterator.get_next()  # Get symbolic data, target tensors
 
     # Build the model
     nb = NaiveBayesModel(w_init=(n_categories, n_dims),
@@ -194,7 +199,7 @@ def train_simple_generative_model_on_mnist(n_categories=20, initial_mag=0.01, op
     train_log_px = nb.log_p_x(x_minibatch)
     train_loss = -1 * tf.reduce_mean(train_log_px)
 
-    test_log_px = nb.log_p_x(x_test)
+    test_log_px = nb.log_p_x(x_test_minibatch)
     test_loss = -1 * tf.reduce_mean(test_log_px)
 
     train_step = nb.train_step(train_loss, flags=(
@@ -203,6 +208,7 @@ def train_simple_generative_model_on_mnist(n_categories=20, initial_mag=0.01, op
     with tf.Session() as sess:
 
         sess.run(train_iterator.initializer)
+        sess.run(test_iterator.initializer)
         sess.run(tf.global_variables_initializer())
 
         # Summary Variables
@@ -225,17 +231,47 @@ def train_simple_generative_model_on_mnist(n_categories=20, initial_mag=0.01, op
                       .format(datetime.now().strftime("%Y-%m-%d %H:%M"), i + 1,
                               int(n_steps), minibatch_size, tr_loss, te_loss))
 
-            # Sample outputs
-            if i % 2000 == 0:
                 samples, sampled_latent_variables = nb.sample(plot_n_samples)
                 nb.plot_samples(samples, sampled_latent_variables, i)
 
-                # Sample outputs for K latent states
-                samples, sampled_latent_variables = nb.sample_K_latent_variables()
-                nb.plot_K_samples(samples, sampled_latent_variables, i)
+                summary.add_summary(summary_train_loss, i)
+                summary.add_summary(summary_test_loss, i)
 
-            summary.add_summary(summary_train_loss, i)
-            summary.add_summary(summary_test_loss, i)
+        # Sample outputs for K latent states
+        samples, sampled_latent_variables = nb.sample_K_latent_variables()
+        nb.plot_K_samples(samples, sampled_latent_variables, i)
+
+        idx = np.random.choice(x_train.shape[0], 10)
+        Normal = x_train[idx]
+        h1, h2 = np.array_split(Normal, 2, axis=1)
+        h2 = np.roll(h2, 1, axis=0)
+        Frankenstein = np.hstack((h1, h2))
+
+        loss_normal = -nb.log_p_x(Normal)
+        loss_frankenstein = -nb.log_p_x(Frankenstein)
+
+        l_normal = -loss_normal.eval()
+        l_frankenstein = -loss_frankenstein.eval()
+
+        plt.figure()
+        for j in range(10):
+            plt.subplot(2, 5, j + 1)
+            plt.imshow(np.reshape(Normal[j], (28, 28)), cmap='gray')
+            plt.title(l_normal[j])
+            plt.axis('off')
+        plt.tight_layout()
+        plt.savefig('Normal.png')
+        plt.close()
+
+        plt.figure()
+        for j in range(10):
+            plt.subplot(2, 5, j + 1)
+            plt.imshow(np.reshape(Frankenstein[j], (28, 28)), cmap='gray')
+            plt.title(l_frankenstein[j])
+            plt.axis('off')
+        plt.tight_layout()
+        plt.savefig('Frankenstein.png')
+    plt.close()
 
 if __name__ == '__main__':
     train_simple_generative_model_on_mnist()
